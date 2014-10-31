d80fd5945f68f9c090f5666f98f400bd5e374e0a
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2014-10-31 11:33:23 +0100

    Version 0.2.21.1 (testing version, will not be released as this version)

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 import os
 
+def read(filename):
+    return file(filename).read()
 
 setup(
     name='diagram',
-    version='0.1',
+    version='0.2.21.1',
     description='Text mode diagrams using UTF-8 characters and fancy colors',
+    long_description=read('README.txt'),
+    url='https://github.com/tehmaze/diagram',
     author='Wijnand Modderman-Lenstra',
     author_email='maze@pyth0n.org',
+    keywords=['diagram', 'graph', 'ascii', 'ansi', 'text'],
     py_modules=['diagram'],
     scripts=[os.path.join('bin', 'diagram')],
 )
