35bf850bd6992f84001d33ca9f2a3d7ecaa85180
Lucas Cimon <lucas.cimon@gmail.com>
2018-12-07 11:28:47 +0100

    Bugfix: fixing screen rendering on some edge case + minor code refactoring + clarified --help

---
+++ test/test_random.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o pipefail -o errexit -o nounset -o xtrace
+
+cd $(dirname "${BASH_SOURCE[0]}")/..
+
+./bin/random 100 | python diagram.py
+./bin/random 100 | python diagram.py -H
+./bin/random 100 | python diagram.py -V
+./bin/random 100 | python diagram.py -b
+./bin/random 100 | python diagram.py -bH
+./bin/random 100 | python diagram.py -bV
+
+# We check that the number of lines in the output is [input size] + 1
+[ $(echo -e '1\n2\n3\n2' | python diagram.py -H | wc -l | awk '{print $1}') -eq 5 ] || exit 1
