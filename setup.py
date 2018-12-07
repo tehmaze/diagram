b0dbb52ff64dbbe27e521013ab13eee2448068f2
tehmaze <tehmaze@users.noreply.github.com>
2018-12-07 12:32:12 +0100

    Hard code long description
    Reading README.md from setup.py is a bad idea, in stead, we'll hardcore a (shorter) long_version.
    
    Fixes #15

--- setup.py
+++ setup.py
@@ -5,11 +5,22 @@ import os
 def read(filename):
     return open(filename).read()
 
+long_description = '''
+Text mode diagrams using UTF-8 characters and fancy colors (using Python).
+
+Features
+ * Axial graphs
+ * Horizontal and vertical bar graphs
+ * Supports both 3 bit (16 color) and 8 bit (256 color) mode colors
+ * Supports various pre-defined palettes
+ * UTF-8 text graphics
+'''.strip()
+
 setup(
     name='diagram',
     version='0.2.25',
     description='Text mode diagrams using UTF-8 characters and fancy colors',
-    long_description=read('README.md'),
+    long_description=long_description,
     url='https://github.com/tehmaze/diagram',
     author='Wijnand Modderman-Lenstra',
     author_email='maze@pyth0n.org',
